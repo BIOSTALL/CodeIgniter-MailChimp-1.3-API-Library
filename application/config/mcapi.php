08d685f3c1d3f5f665273200c0e3dfce0067c2dd
Steve Marks <info@biostall.com>
2011-05-15 17:08:41 +0100

    first commit of config file

---
+++ application/config/mcapi.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// You MailChimp API Key
+// Obtained by logging into your MailChimp account and navigating to 'Account > API Keys and Info'
+$config['mcapi_apikey'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+$config['mcapi_secure'] = false;
